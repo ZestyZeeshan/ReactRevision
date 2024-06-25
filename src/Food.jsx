a7ba578aeb3277c28f92bd83f143cf50828f6a6a
ZestyZeeshan <mohdzeeshanmohammad@gmail.com>
2024-06-25 12:02:57 +0530

    Initial commit

---
+++ src/Food.jsx
@@ -0,0 +1,19 @@
+function Food(){
+
+    const Food1 = "orange";
+    const Food2 = "Banana";
+   
+    return(
+        
+        <ul>
+            <li>Apple</li>
+            <li>{Food1}</li>
+            <li>{Food2.toUpperCase()}</li>
+
+        </ul>
+        
+        
+    );
+}
+
+export default Food;
---
+++ src/Header.jsx
@@ -0,0 +1,32 @@
+
+function Header(){
+    return(
+
+        <>
+         <h1>React Why?</h1>
+     <nav>
+        <ul>
+            <li> <a href='#'>
+                Home</a>
+            </li>
+            <li><a href='#'>
+            Contact</a>
+               
+            </li>
+            <li><a href='#'>
+            AboutUs</a>
+               
+            </li>
+            <li><a href='#'>
+            Food</a>
+             
+            </li>
+        </ul>
+
+     </nav>
+     <hr></hr>
+     </>
+    
+    );
+}
+export default Header;
