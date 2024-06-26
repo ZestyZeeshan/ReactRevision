44c8426437942fbfe9222042ab9f1ea1c6f4b62f
ZestyZeeshan <mohdzeeshanmohammad@gmail.com>
2024-06-26 09:41:53 +0530

    Map and Filter in List

--- src/RenderList/List.jsx
+++ src/RenderList/List.jsx
@@ -5,11 +5,19 @@ function List(){
         {id:3, name:'seuu', age:34},
     ]
 
-    const items= students.map(student => <li key={student.id}>{student.name}:
-                                         &nbsp; {student.age}</li>)
+    const UnderAge = students.filter(student => student.age >25 )
+
+    const items= UnderAge.map(UnderAge => <li key={UnderAge.id}>{UnderAge.name}:
+                                         &nbsp; {UnderAge.age}</li>)
+    
+    //const items= students.map(student => <li key={student.id}>{student.name}:
+    //                                      &nbsp; {student.age}</li>)
+       
 
     //Sort
-    students.sort((a,b) => a.age - b.age)
+    //students.sort((a,b) => a.age - b.age)
+    // For Reverse 
+    //students.sort((a,b) => b.age - a.age)
 
     return(<ul>{items}</ul>);
 }
