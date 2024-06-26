1efe7e56be9b096b7cd52c8027c0140ea427d9a1
ZestyZeeshan <mohdzeeshanmohammad@gmail.com>
2024-06-26 10:53:38 +0530

    Click Events

--- src/Botton/Button.jsx
+++ src/Botton/Button.jsx
@@ -1,10 +1,29 @@
 import styles from './ButtonStyle.module.css'
 
 function Button(){
+
+  let count =0;
+  const handleClick=(name)=>{
+    if(count < 3){
+      count++;
+      console.log(`${name} you clicked me ${count} times `);
+    }else{
+      console.log(`${name} stopp clicking me`)
+    }
+  };
+
+  const handleClick2 = (e) => e.target.textContent="OUCH😏"
+  
     return (
+       <>
+         <button className={styles.button} onClick={()=>
+        handleClick("zesty")}>
+      Click me</button>
+        
 
-      <button className={styles.button}>Click me</button>
-
+        <button onDoubleClick={(e)=> handleClick2(e)}>Clickme</button>
+       </>
+    
     )
 }
 
